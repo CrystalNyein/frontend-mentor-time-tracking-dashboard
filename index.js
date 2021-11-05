c0705073a3d126c76d99f378fb65ace244f59f39
CrystalNyein <nyeinchanlay99@gmail.com>
2021-11-05 18:29:56 +0630

    Data load succesfully based on report state

---
+++ index.js
@@ -0,0 +1,126 @@
+const data = [
+  {
+    title: "Work",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
+      },
+      weekly: {
+        current: 32,
+        previous: 36,
+      },
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
+  },
+  {
+    title: "Play",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
+      },
+      weekly: {
+        current: 10,
+        previous: 8,
+      },
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
+  },
+  {
+    title: "Study",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 7,
+      },
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
+  },
+  {
+    title: "Exercise",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 5,
+      },
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
+  },
+  {
+    title: "Social",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
+      },
+      weekly: {
+        current: 5,
+        previous: 10,
+      },
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
+  },
+  {
+    title: "Self Care",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 2,
+        previous: 2,
+      },
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
+];
+const cardArr = document.getElementsByClassName("card");
+const dailyBtn = document.getElementById("daily-btn");
+const weeklyBtn = document.getElementById("weekly-btn");
+const monthlyBtn = document.getElementById("monthly-btn");
+
+const changeReportState = (state) => {
+  console.log();
+  data.forEach((d, i) => {
+    cardArr[i + 1].querySelector(".current").innerHTML =
+      d.timeframes[state].current + "hrs";
+
+    cardArr[i + 1].querySelector(".previous").innerHTML =
+      "Last Week - " + d.timeframes[state].previous + "hrs";
+  });
+};
+window.onload = (e) => {
+  changeReportState("weekly");
+};
+
+dailyBtn.addEventListener("click", () => changeReportState("daily"));
+weeklyBtn.addEventListener("click", () => changeReportState("weekly"));
+monthlyBtn.addEventListener("click", () => changeReportState("monthly"));
